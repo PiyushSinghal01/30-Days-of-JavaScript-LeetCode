3c0f28ad85d2b41163337d339371ec40c3662b5e
Piyush Singhal <singhalpiyush789@gmail.com>
2024-09-01 23:14:05 +0530

    Question-8 (Day-8)

---
+++ 2629. Function Composition.js
@@ -0,0 +1,21 @@
+var compose = function(functions) {
+    return function(x) {
+
+        if(functions.length > 0)
+        {
+            let ans = x;
+            for(let i = functions.length-1; i >= 0; i--)
+            {
+                lstFn = functions[i];
+                ans = lstFn(ans);
+            }
+
+            return ans;
+        }
+
+        return x;
+    }
+};
+
+const fn = compose([x => x + 1, x => 2 * x])
+console.log(fn(4)); //9
