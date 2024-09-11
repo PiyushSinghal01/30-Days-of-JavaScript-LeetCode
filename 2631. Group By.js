041895e45ac4cc647754e09a2196cc76d958d069
Piyush Singhal <singhalpiyush789@gmail.com>
2024-09-11 22:45:46 +0530

    Question-17 (Day-17)

---
+++ 2631. Group By.js
@@ -0,0 +1,11 @@
+Array.prototype.groupBy = function(fn) {
+    let arr = this;
+    let groupedObj = {};
+    for(let i = 0; i<arr.length; i++){
+        let key = fn(arr[i]);   
+        groupedObj[key] ? groupedObj[key].push(arr[i]) :  groupedObj[key] = [arr[i]];
+    }
+    return groupedObj;
+};
+
+console.log([1,2,3].groupBy(String)) // {"1":[1],"2":[2],"3":[3]}
